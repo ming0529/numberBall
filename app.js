a293a43ee30b27286da236f165d3eedabcc1d32f
서민정 <121smmmj@gmail.com>
2023-08-15 16:08:58 +0900

    숫자야구수정함

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-let num = 0;
 let arr = [];
 
 while(arr.length<3){
-    num =  Math.floor(Math.random()*10);
+    let num =  Math.floor(Math.random()*10);
     if(!arr.includes(num)) arr.push(num);
 }
 
-let count =0;
 alert('컴퓨터가 숫자를 생성함');
+
+let count =0;
 let result =``;
 
 while(true){
-    let ball =0;//같은 숫자
-    let strike =0; //같은 위치에 같은 숫자
+    let ball =0;
+    let strike =0; 
     count++;
-
 
     let user = prompt(`${count} 번째 숫자를 입력하세요 :`);
 
-    user = user.split('').map((x)=>Number(x));//[1,2,3]
+    user = user.split('').map((x)=>Number(x));
 
     user.forEach((x, index)=>{
-        if(arr.indexOf(x)===index){//[2,3,4].indexof(2)===0
+        if(arr.indexOf(x)===index){
             strike ++;
         }else if(arr.includes(x)) ball++;
     
@@ -30,11 +29,11 @@ while(true){
     if(strike===3) result = `${strike}S`;
     else if(ball===3) result = `${ball}B`;
     else result =`${ball}B${strike}S`;
-    
+
     alert(result);
 
     if(strike===3){
-    alert(count+'번만에 맞히셨습니다. 게임을 종료합니다');
+    alert(count+'번 만에 맞히셨습니다. 게임을 종료합니다');
         break;
     }
 }
